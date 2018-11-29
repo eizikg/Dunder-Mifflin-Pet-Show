fbd183760ad1cd1ce52f1b7435986ff372d0496b
github username <github email address>
2018-11-29 16:21:51 -0500

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,46 @@
 class DogsController < ApplicationController
+
+  before_action :get_dog, only: [:update, :destroy, :show, :edit]
+
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+  @dog = Dog.new
+  end
+
+  def show
+  @all_employees = @dog.employees
+  end
+
+  def edit
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    redirect_to @dog
+  end
+
+  def update
+    @dog.update(dog_params)
+    redirect_to @dog
+  end
+
+  def destroy
+    @dog.destroy
+    redirect_to dogs_path
+  end
+
+
+   private
+
+   def get_dog
+    @dog = Dog.find(params[:id])
+   end
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :age)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,44 @@
 class EmployeesController < ApplicationController
+
+  before_action :get_employee, only: [:update, :delete, :show]
+
+
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+  end
+
+  def edit
+  @employee = Employee.new
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create
+    redirect_to @employee
+  end
+
+  def update
+    @employee.update(employee_params)
+  end
+
+  def destroy
+    @employee.destroy
+  end
+
+private
+
+def get_employee
+  @employee = Employee.find(params[:id])
+end
+
+def employee_params
+  prams.require(:employee).permit(:first_name, :last_name, :title, :alias, :office, :img_url, :dog_id)
+end
+
 end
